e6a98848c9d40da2cc2702f92eadc2bcc3643f85
Trish Ogen <patriciaogen@gmail.com>
2019-05-11 13:16:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,12 +27,6 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  elsif board[index] == "X" || board[index] == "O"
-    true
-  else
-    false
-  end
 end
 
 def valid_move?(board, index)
